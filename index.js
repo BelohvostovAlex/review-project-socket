862f5806435e384de6f9090d28af6eb5b82bea41
Alex <belohvostov.belohvostov@gmail.com>
2022-12-20 22:56:32 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const dotenv = require("dotenv").config();
+
+const PORT = process.env.PORT || 7000;
+
+const io = require("socket.io")(PORT, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("sendComment", ({ sender, review, text }) => {
+    io.emit("getComment", { sender, review, text });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
